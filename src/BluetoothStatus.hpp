dc7c91c89eb06876f33a6a90c77ea8ae5435821f
= <=>
2020-05-14 14:34:06 -0700

    created a space for keeping my bluetooth status stuff

---
+++ src/BluetoothStatus.hpp
@@ -0,0 +1,16 @@
+#ifndef _BLUETOOHSTATUS_HPP
+#define _BLUETOOHSTATUS_HPP
+#include "Arduino.h"
+#include <mbed.h> 
+#include "ble/BLE.h"
+
+class ApartmentService{
+    typedef ApartmentService Self; 
+    
+    public: 
+
+    private: 
+
+};
+
+#endif 
