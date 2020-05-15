766db3a582f8d498f7c0969b663a1427ca15fb52
= <=>
2020-05-14 21:12:17 -0700

    Added some documentation

--- src/AniMatrix.cpp
+++ src/AniMatrix.cpp
@@ -1,5 +1,10 @@
 #include "AniMatrix.hpp"
 
+/**************************************************************************/
+/*!
+    @brief Initialization for our matrix animation handler, requires a matrix to manipulate which is passed in as a pointer. 
+*/
+/**************************************************************************/
 void AniMatrix::MatrixAnimationHandler::begin(Adafruit_IS31FL3731 *matrix_ptr){
     this->matrix_ptr = matrix_ptr;
 }
--- src/AniMatrix.hpp
+++ src/AniMatrix.hpp
@@ -12,6 +12,11 @@
 
 namespace AniMatrix{
 
+/**************************************************************************/
+/*!
+    @brief Animation Handler that should make dealing with matrix animations easier and reliant on the rtos. 
+*/
+/**************************************************************************/
 class MatrixAnimationHandler{
     public: 
         void begin(Adafruit_IS31FL3731 *matrix_ptr);
